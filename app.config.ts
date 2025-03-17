b5fc5b97675f1b8e0c44fcb7b7a190e58267b831
GobsRuiz <gbibisruiz@hotmail.com>
2025-03-17 20:26:08 -0300

    chore: configure buttons in app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,13 +1,15 @@
 export default defineAppConfig({
   ui: {
-    strategy: 'override',
+    button: {
+      color: {
+        transparent: {
+          solid: 'bg-transparent hover:bg-customBlue-300 hover:border-customBlue-300 text-customWhite-100 border-2 rounded-full text-sm font-bold py-1 px-5'
+        },
 
-    // button: {
-    //   color: {
-    //     white: {
-    //       solid: 'bg-red-500 dark:bg-gray-900'
-    //     },
-    //   },
-    // },
+        link: {
+          solid: 'bg-transparent hover:bg-transparent text-customBlue-400 hover:text-customBlue-500 border-0 text-sm font-bold py-1 px-5'
+        },
+      },
+    },
   },
 });
