c177dc271b0fbe005a6e71856125ffd7d72ef27b
GobsRuiz <gbibisruiz@hotmail.com>
2025-03-17 20:27:11 -0300

    refactor: removed unnecessary code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,14 +13,7 @@ export default defineNuxtConfig({
 
   css: [
     '@/assets/css/tailwind.css',
+    '@/assets/css/fonts.css',
+    '@/assets/css/reset.css',
   ],
-
-  googleFonts: {
-    families: {
-      Inter: [200, 300, 400, 500, 600, 700, 800, 900],
-    },
-
-    display: 'swap',
-    preconnect: true,
-  },
 })
