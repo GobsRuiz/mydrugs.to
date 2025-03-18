41ce60abe77f119930e837559068940e61884ec2
GobsRuiz <gbibisruiz@hotmail.com>
2025-03-18 17:15:54 -0300

    Feat: I put a minHeight calc for the main tag

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,40 @@
   <div>
     <LayoutsHeaderComponent />
 
-    <main class="my-7">
+    <main class="main my-7" :style="mainHeight">
         <slot />
     </main>
 
     <LayoutsFooterComponent />
   </div>
 </template>
+
+<script setup>
+const screenWidth = ref(0)
+const mainHeight = computed(() => screenWidth.value > 991 ? { minHeight: '747px' } : {} )
+
+function setHeightToMainTag()
+{
+  const header = document.querySelector("header")
+  const main = document.querySelector("main")
+  const footer = document.querySelector("footer")
+
+  if(header, main, footer){
+    main.style.minHeight = (window.innerHeight - (header.offsetHeight + footer.offsetHeight + 56)) + "px"
+  }
+}
+
+function eventResize()
+{
+  window.addEventListener("resize", () => {
+    screenWidth.value = innerWidth
+  })
+}
+
+onMounted(() => {
+  screenWidth.value = innerWidth
+  
+  setHeightToMainTag()
+  eventResize()
+})
+</script>
