3e8f9fb602e8d3715b77e80637e74ab08adb1a87
GobsRuiz <gbibisruiz@hotmail.com>
2025-03-18 12:30:39 -0300

    Feat: product page and ProductCard component. It's not finished yet. I still need to work on the idea, I want to create other pages to better understand the objective

---
+++ components/ui/ProductCardComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="productCard flex items-center justify-center border-2 border-customWhite-600 rounded-lg py-4 px-5">
+    <img class="max-w-[80px] h-max mr-7" src="/assets/images/drugs/blue_pill.png" alt="Imagem ilustrativa">
+
+    <div class="productCard__infos">
+        <h3 class="text-customWhite-100 text-lg leading-5 font-bold mb-2">
+            Red<br>
+            Heart
+        </h3>
+        <p class="text-customWhite-400 text-sm font-semibold italic">
+            0.0017 BTC
+        </p>
+        <img class="max-w-[70px]" src="/assets/images/ratings.png" alt="rating">
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.productCard{
+
+}
+</style>
---
+++ pages/shop/[drug].vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="productPage max-w-[1300px] mx-auto box-border px-5">
+    <div class="productPage__product grid gap-10 lg:grid-cols-[minmax(200px,300px)_minmax(350px,400px)_300px] sm:grid-cols-[minmax(280px,400px)_minmax(250px,300px)] grid-cols-1 lg:items-center lg:justify-between md:justify-center mt-7">
+        <div class="productPage__product__image lg:block hidden lg:col-start-auto md:col-start-1">
+            <img class="w-full" src="/assets/images/drugs/grenade-pill.png" alt="Droga">
+        </div>
+
+        <div class="productPage__product__about col-span-1 lg:col-start-2 md:col-start-1 md:row-start-1 sm:max-w-max max-w-[300px]">
+            <p class="text-customBlue-300 text-xs font-bold ml-1 -m-1">NEW</p>
+            <h1 class="text-customWhite-100 lg:text-6xl md:text-4xl sm:text-4xl text-3xl font-bold -mb-1">
+                GRENADE <br>
+                PILL
+            </h1>
+            <p class="text-customWhite-400 text-2xl font-bold mb-2">
+                Premium Line
+            </p>
+            <img class="max-w-[120px] mb-2 pb-1" src="/assets/images/ratings.png" alt="rating">
+            <p class="text-customWhite-100 lg:text-6xl md:text-4xl sm:text-4xl text-3xl font-bold -m-1.5">
+                0.0024 BTC
+            </p>
+            <p class="text-customWhite-100 text-lg font-bold">
+                24.06 €
+            </p>
+
+            <p class="text-customWhite-400 text-md font-semibold mt-3">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit incidunt blanditiis molestiae ad dolorum, eos numquam! Totam sint vitae inventore incidunt, mollitia culpa corrupti illum?
+            </p>
+        </div>
+
+        <div class="productPage__product__buy border-2 border-customWhite-600 rounded-lg box-border py-4 px-8 lg:h-full md:h-max lg:col-start-3 md:col-start-2 md:row-start-1 sm:max-w-max max-w-[300px]">
+            <h3 class="text-customWhite-100 lg:text-3xl text-2xl font-bold mb-3">
+                Buy now
+            </h3>
+
+            <p class="text-customWhite-100 text-sm font-semibold mb-3">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur cupiditate velit ea debitis incidun.
+            </p>
+
+            <p class="text-customWhite-400 text-xs font-semibold">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </p>
+            <p class="text-primary text-sm font-bold">
+                In Stock
+            </p>
+
+            <div class="flex items-center justify-between my-4 pb-1">
+                <button class="flex items-center border border-customWhite-100 rounded-full p-2">
+                    <svg class="w-[30px] h-[30px]" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Edit / Remove_Minus"> <path id="Vector" d="M6 12H18" stroke="#f3efee" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>
+                </button>
+
+                <button class="flex items-center border border-customWhite-100 rounded-full p-2">
+                    <UIcon name="lets-icons:add-round" class="w-[30px] h-[30px] text-customWhite-100" />
+                </button>
+            </div>
+
+            <UButton color="transparent" class="block w-full mb-3 text-lg">
+                Add to Cart
+            </UButton>
+            <UButton color="primary" class="block w-full text-lg">
+                Check Out
+            </UButton>
+
+            <p class="text-customWhite-400 text-xs text-center font-semibold mt-3">
+                Lorem ipsum dolor sit.
+            </p>
+        </div>
+    </div>
+
+    <div class="h-[1px] bg-customWhite-400 my-8"></div>
+
+    <div class="grid lg:grid-cols-4 grid-cols-1 gap-6 px-6">
+        <UiProductCardComponent class="max-w-[300px]" />
+        <UiProductCardComponent class="max-w-[300px]" />
+        <UiProductCardComponent class="max-w-[300px]" />
+        <UiProductCardComponent class="max-w-[300px]" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+
+console.log(route)
+</script>
+
+<style lang="scss" scoped>
+
+</style>
