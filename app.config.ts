c307e6d4cc65ddc906d6b8979062e838ff1a56ac
GobsRuiz <gbibisruiz@hotmail.com>
2025-03-18 12:30:57 -0300

    Feat: new config

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,10 @@ export default defineAppConfig({
   ui: {
     button: {
       color: {
+        primary: {
+          solid: 'bg-customBlue-300 hover:bg-customBlue-300 hover:border-customBlue-300 text-customWhite-100 border-2 border-customBlue-300 rounded-full text-sm font-bold py-1 px-5'
+        },
+
         transparent: {
           solid: 'bg-transparent hover:bg-customBlue-300 hover:border-customBlue-300 text-customWhite-100 border-2 rounded-full text-sm font-bold py-1 px-5'
         },
