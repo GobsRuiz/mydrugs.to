27839e1c4135e5a4f6fbf43e618d7bbcf46369fe
GobsRuiz <gbibisruiz@hotmail.com>
2025-03-18 16:29:31 -0300

    Feat: contact page, then I work on the page

---
+++ pages/contact.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="contact">
+    <h1 class="text-center text-customWhite-100 text-4xl font-bold mb-8">
+        Contact
+    </h1>
+
+    <p>
+        gobs.ruiz
+    </p>
+  </section>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
