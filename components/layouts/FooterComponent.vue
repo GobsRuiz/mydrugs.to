1df7fa5e68d6211ef7711572fe90ceb01b393449
GobsRuiz <gbibisruiz@hotmail.com>
2025-03-18 16:37:38 -0300

    Feat: footer

---
+++ components/layouts/FooterComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="footer max-w-[1300px] border-t border-customWhite-400 mx-auto py-4 px-4">
+    <p class="text-center text-customWhite-100 text-xs font-bold mb-3">
+        A website inspired by the series <span class="italic">How To Sell Drugs Online</span>
+    </p>
+    <p class="text-center text-customWhite-100 text-xs font-bold">
+        Developed by: <a class="text-customBlue-300" href="http://gobsruiz.com.br/" target="_blank">gobs.ruiz</a>
+    </p>
+  </footer>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.footer{
+
+}
+</style>
