5426489e3b486229d46d732686bb0f68be851d93
GobsRuiz <gbibisruiz@hotmail.com>
2025-03-18 15:07:16 -0300

    feat: shop page

--- pages/shop/index.vue
+++ pages/shop/index.vue
@@ -1,13 +1,46 @@
 <template>
-  <div>
-    <p>
-        index
-    </p>
+  <div class="max-w-[1300px] mx-auto mt-7 box-border px-4">
+    <h1 class="sm:text-left text-center text-customWhite-100 text-4xl font-bold mb-8">
+        Shop
+    </h1>
+
+    <div v-if="products.length > 0" class="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-8">
+        <UiProductCardComponent 
+            v-for="(product, index) in products" :key="index"
+            class="sm:max-w-full max-w-[300px] mx-auto"
+            :product="product" 
+        />
+    </div>
   </div>
 </template>
 
 <script setup>
+// Imports
+import { useProducts } from '~/composables/useProducts.js'
 
+
+// Global variables
+const toast = useToast()
+
+
+// Get products
+const products = ref([])
+const reqError = ref(null)
+
+try {
+  products.value = await useProducts('/api/products')
+  console.log(products)
+} 
+catch (error) {
+    reqError.value = error
+    console.log(error)
+
+    toast.add({
+        title: 'error',
+        description: error.statusMessage,
+        color: 'red'
+    })
+}
 </script>
 
 <style lang="scss" scoped>
