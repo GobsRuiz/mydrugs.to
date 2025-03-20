cc28b061397e32f94ebcc1932c681268237b962f
GobsRuiz <gbibisruiz@hotmail.com>
2025-03-19 21:05:58 -0300

    Feat: links on contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,12 +1,45 @@
 <template>
-  <section class="contact">
-    <h1 class="text-center text-customWhite-100 text-4xl font-black mb-8">
+  <section class="contact max-w-[1300px] mx-auto box-border px-4">
+    <h1 class="text-center text-customWhite-100 lg:text-4xl text-2xl font-black mb-8">
       Contact
     </h1>
 
-    <p>
-      gobs.ruiz
-    </p>
+    <div class="grid sm:grid-cols-[repeat(3,160px)] grid-cols-[repeat(2,minmax(130px,160px))] gap-4 justify-center">
+      <NuxtLink to="https://gobsruiz.com.br/#projects" target="_blank" class="flex items-center justify-center bg-transparent hover:bg-customBlue-500 text-customWhite-100 hover:text-customBunker-950 border-2 border-customBorderColor-default hover:border-customBlue-500 rounded-lg text-sm font-bold py-3 px-2">
+        <UIcon name="formkit:file" class="w-4 h-4 mr-2" />
+        Portfólio
+      </NuxtLink>
+
+      <NuxtLink to="https://www.youtube.com/@gobsruizdev" target="_blank" class="w-full flex items-center justify-center bg-transparent hover:bg-customBlue-500 text-customWhite-100 hover:text-customBunker-950 border-2 border-customBorderColor-default hover:border-customBlue-500 rounded-lg text-sm font-bold py-3 px-2">
+        <UIcon name="mdi:youtube" class="w-4 h-4 mr-2" />
+        Youtube
+      </NuxtLink>
+
+      <NuxtLink to="https://www.tiktok.com/@gobs.ruiz.dev" target="_blank" class="w-full flex items-center justify-center bg-transparent hover:bg-customBlue-500 text-customWhite-100 hover:text-customBunker-950 border-2 border-customBorderColor-default hover:border-customBlue-500 rounded-lg text-sm font-bold py-3 px-2">
+        <UIcon name="ic:baseline-tiktok" class="w-4 h-4 mr-2" />
+        TikTok
+      </NuxtLink>
+
+      <NuxtLink to="https://www.instagram.com/gobs.ruiz.dev" target="_blank" class="w-full flex items-center justify-center bg-transparent hover:bg-customBlue-500 text-customWhite-100 hover:text-customBunker-950 border-2 border-customBorderColor-default hover:border-customBlue-500 rounded-lg text-sm font-bold py-3 px-2">
+        <UIcon name="ri:instagram-line" class="w-4 h-4 mr-2" />
+        Instagram
+      </NuxtLink>
+
+      <NuxtLink to="https://github.com/GobsRuiz" target="_blank" class="w-full flex items-center justify-center bg-transparent hover:bg-customBlue-500 text-customWhite-100 hover:text-customBunker-950 border-2 border-customBorderColor-default hover:border-customBlue-500 rounded-lg text-sm font-bold py-3 px-2">
+        <UIcon name="mingcute:github-line" class="w-4 h-4 mr-2" />
+        GitHub
+      </NuxtLink>
+
+      <NuxtLink to="https://www.linkedin.com/in/gabriel-eduardo-ruiz-82235a239/" target="_blank" class="w-full flex items-center justify-center bg-transparent hover:bg-customBlue-500 text-customWhite-100 hover:text-customBunker-950 border-2 border-customBorderColor-default hover:border-customBlue-500 rounded-lg text-sm font-bold py-3 px-2">
+        <UIcon name="bx:bxl-linkedin" class="w-4 h-4 mr-2" />
+        Linkedin
+      </NuxtLink>
+
+      <NuxtLink to="https://gobsruiz.com.br/links" target="_blank" class="flex items-center justify-center bg-transparent hover:bg-customBlue-500 text-customWhite-100 hover:text-customBunker-950 border-2 border-customBorderColor-default hover:border-customBlue-500 rounded-lg text-sm font-bold py-3 px-2">
+        <UIcon name="material-symbols:attachment-rounded" class="w-4 h-4 mr-2" />
+        Links
+      </NuxtLink>
+    </div>
   </section>
 </template>
 
